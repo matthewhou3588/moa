a3a6733ff929338f8cb83d5b10a0873be570cb75
matthew <hyfzxs@163.com>
2019-02-15 17:55:53 +0800

    增加WithDBSCAN的测试，提取出核心的与界面无关的代码。跑5000个点后暂定进程

---
+++ moa/src/test/java/moa/clusters/denstream/WithDBSCANTest.java
@@ -0,0 +1,64 @@
+package moa.clusters.denstream;
+
+import moa.core.AutoClassDiscovery;
+import moa.core.AutoExpandVector;
+import moa.evaluation.MeasureCollection;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+public class WithDBSCANTest {
+    private WithDBSCANRun withDBSCANRun = null;
+    private Thread withDBSCANRunThread = null;
+    private Boolean running = false;
+
+
+
+
+    public WithDBSCANTest() {
+
+    }
+
+
+    private void createwithDBSCANRunThread(){
+        withDBSCANRun = new WithDBSCANRun();
+        withDBSCANRunThread = new Thread(withDBSCANRun);
+    }
+
+
+    public void toggleState() {
+        if(withDBSCANRun == null)
+            createwithDBSCANRunThread();
+
+        if(!withDBSCANRunThread.isAlive()){
+            withDBSCANRunThread.start();
+        }
+
+        //pause
+        if(running){
+            running = false;
+            withDBSCANRun.pause();
+            withDBSCANRun.showClusterMetrics();
+        }
+        else{
+            running = true;
+            withDBSCANRun.resume();
+        }
+    }
+
+
+
+
+    public static void main(String[] args) {
+        WithDBSCANTest obj = new WithDBSCANTest();
+        obj.toggleState();
+
+
+//        for (MeasureCollection e : measureCollection) {
+//        Class<?> clazz = (MeasureCollection)Class.forName("moa.evaluation.CMM");
+//            System.out.println(e.class.getName());
+//        }
+
+    }
+
+}
